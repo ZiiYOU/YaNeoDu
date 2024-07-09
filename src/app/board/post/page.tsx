2952e2039e3db50550f4950671b10a5bf52a3ef5
LEE-NS <ogawa9341@gmail.com>
2024-07-09 21:18:19 +0900

    update : [post] style

--- src/app/board/post/page.tsx
+++ src/app/board/post/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Detail() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="flex justify-center items-center">
       <div className="w-[1000px] bg-slate-100 p-10">
         <div className="flex justify-between items-center p-3">
           <h1 className="text-2xl">질문 및 후기</h1>
           <Link
             className="text-gray-200 text-sm text-center w-[120px] pt-2 pb-2 bg bg-[#0090f9] rounded-md transition-all hover:bg-[#0073c6]"
             href={"#"}
+            title="글쓰기"
           >
             글쓰기
           </Link>
@@ -33,7 +34,10 @@ export default function Detail() {
                 <p className="text-gray-400">2024.07.09 12:34:56</p>
               </div>
               <div className="flex items-center gap-3">
-                <button className=" hover:underline hover:text-[#0090f9]">
+                <button
+                  className=" hover:underline hover:text-[#0090f9]"
+                  title="글 수정하기"
+                >
                   수정하기
                 </button>
                 <p className="text-gray-400">
@@ -44,7 +48,20 @@ export default function Detail() {
           </div>
           <p className="h-[1px] w-full ml-auto mr-auto bg-slate-300"></p>
           {/* 게시글 상단 */}
-          <div className="text-sm p-3">내용</div>
+          <div className="text-sm p-3">
+            내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+          </div>
         </div>
         <div className="bg-[#fefefe] p-3 m-3 rounded-md text-sm">
           <div className="flex justify-between items-center p-3">
@@ -52,7 +69,10 @@ export default function Detail() {
               className="bg-gray-100 w-[85%] p-2 text-sm resize-none outline-none overflow-hidden h-[90px]"
               placeholder="내용을 입력해주세요"
             ></textarea>
-            <button className="bg-[#0090f9] rounded-md transition-all w-[120px] hover:bg-[#0073c6] text-gray-200 h-[90px]">
+            <button
+              className="bg-[#0090f9] rounded-md transition-all w-[120px] hover:bg-[#0073c6] text-gray-200 h-[90px]"
+              title="댓글 등록하기"
+            >
               등록하기
             </button>
           </div>
@@ -62,15 +82,31 @@ export default function Detail() {
           <h2 className="p-3">댓글</h2>
           <p className="h-[1px] w-full ml-auto mr-auto bg-slate-300"></p>
           <div className="p-3 border-b border-dotted">
-            <ul className="flex items-center text-sm">
+            <ul className="flex items-center text-sm justify-between text-center">
               <li className="w-[10%]">
                 <span className="font-bold">뉴비</span>&nbsp;님
               </li>
-              <li className="w-[69%]">도배 좀 그만하세요;;</li>
+              <li className="w-[55%] text-left overflow-hidden white">
+                도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배
+                좀 그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;도배 좀 그만하세요;;도배 좀 그만하세요;;도배 좀
+                그만하세요;;
+              </li>
               <li className="w-[20%] text-gray-400">2024.07.08 15:00:09</li>
               <li className="w-[10%] flex justify-evenly">
-                <button className="p-1">✎</button>
-                <button className="p-1">✕</button>
+                <button className="p-1 hover:text-[#0073c6]" title="수정하기">
+                  ✎
+                </button>
+                <button className="p-1 hover:text-[#0073c6]" title="삭제하기">
+                  ✕
+                </button>
               </li>
             </ul>
           </div>
