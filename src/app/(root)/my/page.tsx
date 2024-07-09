69914662011c99587c5e37dafacda4cfd16ba048
Leejunhyeok03 <ly0609@naver.com>
2024-07-10 05:39:35 +0900

    style:Licenses 컴포넌트 자격증 추가 버튼 add

--- src/app/(root)/my/page.tsx
+++ src/app/(root)/my/page.tsx
@@ -3,7 +3,7 @@ import LicensesList from '@/app/components/LicensesList';
 function myPage() {
   return (
     <div>
-      <h3 className="font-bold text-4xl leading-[10rem] text-gray-800 text-center ">OOO님의 자격증 정보</h3>
+      <h3 className="font-bold text-4xl leading-[8rem] mt-3 text-gray-800 text-center ">OOO님의 자격증 정보</h3>
       <LicensesList />
     </div>
   )
--- src/app/components/LicensesList.tsx
+++ src/app/components/LicensesList.tsx
@@ -1,60 +1,110 @@
 "use client";
 
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BsFillXCircleFill } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
+import Select from 'react-select';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
-
-
+type LicenseOption = {
+  value: string;
+  label: string;
+};
 
 function LicensesList() {
+  const [options, setOptions] = useState<LicenseOption[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("/api/licenses");
-        console.log(response.data);
+        const licenses = response.data.map((license: { license_name: string }) => ({
+          value: license.license_name,
+          label: license.license_name,
+        }));
+        setOptions(licenses);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, [])
-  
-  return (
+  }, []);
 
-  
-  <table className='border-collapse table-auto w-[1240px] m-auto border border-slate-300 text-center '>
-    <thead>
-      <tr className='h-12 font-bold text-lg border-b border-slate-300'>
-        <td>자격증 명</td>
-        <td>자격증 번호</td>
-        <td>발급 연월일</td>
-        <td>내지번호</td>
-        <td className='w-[10%]'>확인 유무</td>
-        <td className='w-[5%]'></td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className='h-16'>
-        <td>자격증 명</td>
-        <td>자격증 번호</td>
-        <td>발급 연월일</td>
-        <td>내지번호</td>
-        <td><BsFillXCircleFill className='m-auto' size={40} color={"#d80e0e"}/> </td>
-        {/* <BsCheckCircleFill className='m-auto' size={40} color={"#3e8311"}/> */}
-        <td><MdDeleteForever className='cursor-pointer m-auto' size={30}/></td>
-      </tr>
-    </tbody>
-  </table>
-  )
+  return (
+    <div className='w-[1240px] m-auto'>
+      <button className='px-5 py-2 mb-3 ml-auto bg-[#0090F9] flex items-center text-white font-bold text-center rounded-lg'><FaPlus className='mr-2' color='#ffffff'/> 자격증 추가하기</button>
+      <table className='w-full border-collapse table-fixed  border border-slate-300 text-center '>
+        <thead>
+          <tr className='h-12 font-bold text-lg border-b border-slate-300 bg-gray-50'>
+            <td>자격증 명</td>
+            <td>자격증 번호</td>
+            <td>발급 연월일</td>
+            <td>내지번호</td>
+            <td className='w-[8%]'>확인 유무</td>
+            <td className='w-[5%]'></td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className='h-16'>
+            <td className='px-3 text-left'><Select options={options} placeholder="자격증 목록..."/></td>
+            <td className='px-3'>자격증 번호</td>
+            <td className='px-3'>발급 연월일</td>
+            <td className='px-3'>내지번호</td>
+            <td><BsFillXCircleFill className='m-auto' size={35} color={"#d80e0e"}/> </td>
+            {/* <BsCheckCircleFill className='m-auto' size={35} color={"#3e8311"}/> */}
+            <td><MdDeleteForever className='cursor-pointer m-auto' size={35}/></td>
+          </tr>
+          <tr className='h-16'>
+            <td className='px-3 text-left'><Select options={options} placeholder="자격증 목록..."/></td>
+            <td className='px-3'>자격증 번호</td>
+            <td className='px-3'>발급 연월일</td>
+            <td className='px-3'>내지번호</td>
+            <td><BsFillXCircleFill className='m-auto' size={35} color={"#d80e0e"}/> </td>
+            {/* <BsCheckCircleFill className='m-auto' size={35} color={"#3e8311"}/> */}
+            <td><MdDeleteForever className='cursor-pointer m-auto' size={35}/></td>
+          </tr>
+          <tr className='h-16'>
+            <td className='px-3 text-left'><Select options={options} placeholder="자격증 목록..."/></td>
+            <td className='px-3'>자격증 번호</td>
+            <td className='px-3'>발급 연월일</td>
+            <td className='px-3'>내지번호</td>
+            <td><BsFillXCircleFill className='m-auto' size={35} color={"#d80e0e"}/> </td>
+            {/* <BsCheckCircleFill className='m-auto' size={35} color={"#3e8311"}/> */}
+            <td><MdDeleteForever className='cursor-pointer m-auto' size={35}/></td>
+          </tr>
+          <tr className='h-16'>
+            <td className='px-3 text-left'><Select options={options} placeholder="자격증 목록..."/></td>
+            <td className='px-3'>자격증 번호</td>
+            <td className='px-3'>발급 연월일</td>
+            <td className='px-3'>내지번호</td>
+            <td><BsFillXCircleFill className='m-auto' size={35} color={"#d80e0e"}/> </td>
+            {/* <BsCheckCircleFill className='m-auto' size={35} color={"#3e8311"}/> */}
+            <td><MdDeleteForever className='cursor-pointer m-auto' size={35}/></td>
+          </tr>
+          <tr className='h-16'>
+            <td className='px-3 text-left'><Select options={options} placeholder="자격증 목록..."/></td>
+            <td className='px-3'>자격증 번호</td>
+            <td className='px-3'>발급 연월일</td>
+            <td className='px-3'>내지번호</td>
+            <td><BsFillXCircleFill className='m-auto' size={35} color={"#d80e0e"}/> </td>
+            {/* <BsCheckCircleFill className='m-auto' size={35} color={"#3e8311"}/> */}
+            <td><MdDeleteForever className='cursor-pointer m-auto' size={35}/></td>
+          </tr>
+          <tr className='h-16'>
+            <td className='px-3 text-left'><Select options={options} placeholder="자격증 목록..."/></td>
+            <td className='px-3'>자격증 번호</td>
+            <td className='px-3'>발급 연월일</td>
+            <td className='px-3'>내지번호</td>
+            <td><BsFillXCircleFill className='m-auto' size={35} color={"#d80e0e"}/> </td>
+            {/* <BsCheckCircleFill className='m-auto' size={35} color={"#3e8311"}/> */}
+            <td><MdDeleteForever className='cursor-pointer m-auto' size={35}/></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
-{/* <Select options={options} /> */}
-export default LicensesList
+
+export default LicensesList;
