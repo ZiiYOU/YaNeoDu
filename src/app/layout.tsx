b89e09d23fa57baff78b2d612144b7b7af4fd512
Jiyoung <jiyoung990602@gmail.com>
2024-07-09 20:41:58 +0900

    💄 style: main Page UI 작업 완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className= 'relative flex flex-col items-center'>
+      <body className= 'relative mt-16 flex flex-col items-center'>
         <header className="fixed top-0 px-12 w-full h-16 bg-white border border-solid border-b-default-color drop-shadow-md flex flex-row items-center justify-between">
-          <div className="text-lg font-bold text-theme-color drop-shadow-md">you can do it!</div>
+          <div className="text-lg font-bold text-theme-color drop-shadow-md cursor-pointer">you can do it!</div>
           <div className="flex flex-row gap-3">
             <button className="w-20 h-7 border border-solid border-theme-color bg-theme-color rounded-lg text-sm text-white drop-shadow-md hover:bg-white hover:text-theme-color ease-in duration-300">회원가입</button>
             <button className="w-20 h-7 border border-solid border-theme-color bg-white rounded-lg text-sm text-theme-color drop-shadow-md hover:bg-theme-color hover:text-white ease-in duration-300 ">로그인</button>
           </div>
-          
         </header>
         {children}
       </body>
