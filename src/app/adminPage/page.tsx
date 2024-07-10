2f5a4c6a77ed91aa78ac28fee6add57ff632967b
hyowls <hhvucyh@gmail.com>
2024-07-10 19:51:19 +0900

    승인 상태 별 필터링

--- src/app/adminPage/page.tsx
+++ src/app/adminPage/page.tsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from 'react'
 
 const Page = () => {
   const [licenses, setLicenses] = useState<CheckLicense>([])
-
+  const [filterData, setFilterData] = useState('pending')
   // supabase데이터 가져오기
   useEffect(()=> {
     const fetchData = async () => {
@@ -21,7 +21,7 @@ const Page = () => {
     fetchData()
   },[])
 
-  // supabase 승인 상태 업데이트
+  // supabase 승인 상태 업데이트 & 승인 일자 업데이트
   const handleConfirm = async(id : string, is_confirm : boolean) => {
     const confirmDate = new Date().toISOString().split('T')[0]
     const {data, error} = await supabase
@@ -40,11 +40,33 @@ const Page = () => {
     }
   }
 
+  // 승인 여부 필터링
+  const filteredLicenses = licenses.filter((license)=> {
+    if(filterData === 'pending') return !license.is_confirm
+    if(filterData === 'confirmed') return license.is_confirm
+    return true
+  })
+
+
 
   return (
     <div className='max-w-[1300px]'>
       <h1 className='mx-[120px] mt-[50px] text-3xl'>진위확인 신청 목록</h1>
-      <div className='border border-solid w-[1200px] mx-auto mt-[40px] p-3 rounded-xl'>
+      <div className='flex justify-first mt-[30px]'>
+        <button 
+          className={`border-x border-t border-solid py-2 px-4 rounded ${filterData === 'pending'? 'bg-[#0090F9] text-white' : 'bg-white'}`}
+          onClick={()=> setFilterData('pending')}
+        >
+          승인 대기중
+        </button>
+        <button 
+          className={`border-x border-t py-2 px-4 border border-solid rounded ${filterData === 'confirmed'? 'bg-[#0090F9] text-white' : 'bg-white'}`}
+          onClick={()=> setFilterData('confirmed')}
+        >
+          승인 완료
+        </button>
+      </div>
+      <div className='border border-solid w-[1200px] mx-auto p-3 rounded-tr-xl rounded-br-xl rounded-bl-xl mb-[50px]'>
         <table className='w-full border-collapse'>
           <thead>
             <tr className='border-b border-solid'>
@@ -59,7 +81,28 @@ const Page = () => {
             </tr>
           </thead>
           <tbody>
-              {
+            {
+              filteredLicenses.map((license)=> (
+                <tr className='border-b border-solid' key={license.id}>
+                  <td className='p-[18px] text-center'>{license.user_id}</td>
+                  <td className='p-[18px] text-center'>{license.user_name}</td>
+                  <td className='p-[18px] text-center'>{license.user_birth}</td>
+                  <td className='p-[18px] text-center'>{license.license_number}</td>
+                  <td className='p-[18px] text-center'>{license.license_issue}</td>
+                  <td className='p-[18px] text-center'>{license.license_sub_number}</td>
+                  <td className='p-[18px] text-center'>{license.confirm_date}</td>
+                  <td>
+                    <button 
+                      className={`border border-solid py-[7px] px-[10px] rounded-xl text-white ${license.is_confirm ? 'bg-[#FF3030]' : 'bg-[#0090F9]'}`}
+                      onClick={() => handleConfirm(license.id, license.is_confirm)}
+                    >
+                      {license.is_confirm? '승인취소' : '승인확인'}
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+              {/* {
                 licenses.map((license)=>(
                 <tr className='border-b border-solid' key={license.id}>
                   <td className='p-[18px] text-center'>{license.user_id}</td>
@@ -79,7 +122,7 @@ const Page = () => {
                   </td>
                 </tr>
                 ))
-              }
+              } */}
           </tbody>
         </table>
       </div>
