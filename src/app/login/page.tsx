132743b22697faeb5f2f6cd61b8212f1772eac57
mini-woong <mini_0@inu.ac.kr>
2024-07-11 14:09:41 +0900

    add, style: 로그인, 회원가입 css

---
+++ src/app/login/page.tsx
@@ -0,0 +1,71 @@
+import "@/app/globals.css";
+
+export default function Login() {
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                이메일
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  비밀번호
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-theme-color hover:text-sky-400">
+                    비밀번호를 까먹으셨나요?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-theme-color px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ease-out duration-100"
+              >
+                로그인
+              </button>
+            </div>
+          </form>
+
+          <p className="w-80 mt-5 text-center text-sm text-gray-500">
+            아직 계정이 없으신가요?{' '}
+            <a href="/signup" className="font-semibold leading-6 text-theme-color hover:text-sky-400">
+              회원가입
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+
+}
