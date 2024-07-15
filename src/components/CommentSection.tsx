2df0312b5d083636ab7b548a8dd1011fda84d25e
LEE-NS <ogawa9341@gmail.com>
2024-07-15 13:50:25 +0900

    update : 일부 유효성 검사 추가

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -10,7 +10,7 @@ export default function CommentSection({paramsId}: Props) {
   return (
     <>
       <CommentWrite paramsId={paramsId} />
-      <div className="bg-[#fefefe] p-3 m-3 rounded-md min-h-[200px]">
+      <div className="bg-[#fefefe] p-3 m-3 rounded-md">
         <h2 className="p-3">댓글</h2>
         <p className="h-[1px] w-full ml-auto mr-auto bg-slate-300"></p>
         <Comment paramsId={paramsId} />
